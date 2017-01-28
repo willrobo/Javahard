107557063d2253fe26f0605c20f017a675ab23db
Will Roberts <will@Wills-MacBook-Air.local>
2017-01-28 13:13:51 -0700

    INitial commit

---
+++ javahard/FirstProg.java
@@ -0,0 +1,8 @@
+public class FirstProg 
+{
+	public static void main( String [] args ) 
+	{
+		System.out.println( "I, Will Roberts, am determined to learn how to code." );
+		System.out.println( "Today's date is Saturday, January 28th, 2017." );
+	}
+}
